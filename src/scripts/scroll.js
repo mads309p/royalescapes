93c9d5205010c8676063435c690052a7cd0ecc87
mads309p <madsvolke@gmail.com>
2023-04-13 10:22:10 +0200

    små ændringer

---
+++ src/scripts/scroll.js
@@ -0,0 +1,13 @@
+const images = document.querySelectorAll(".scroll-image, .scroll-image2, .scroll-image3, .scroll-image4");
+
+window.addEventListener("scroll", () => {
+  const scrollPosition = window.scrollY;
+  images.forEach((image) => {
+    if (scrollPosition > 100) {
+      /* change this value to adjust the scroll position */
+      image.classList.add("active");
+    } else {
+      image.classList.remove("active");
+    }
+  });
+});
